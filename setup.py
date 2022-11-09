e88f3b7c31d297b6a800065937a7f05d5421b894
Sam Partee <sam.partee@redis.com>
2022-11-08 17:40:44 -0800

    make it into a python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+# function to read in requirements.txt to into a python list
+def read_requirements():
+    with open("requirements.txt") as f:
+        requirements = f.read().splitlines()
+    return requirements
+
+setup(
+    name="redisvl",
+    description="Vector loading utility for Redis vector search",
+    license="BSD-3-Clause",
+    version="0.1.0",
+    install_requires=read_requirements(),
+    packages=["redisvl"],
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "redisvl = redisvl.cli.__init__:main"
+        ]
+    }
+)
