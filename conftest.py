138d9f58d6a6e609fad610b1d4cdd5e77dc8c9bb
Sam Partee <sam.partee@redis.com>
2022-12-02 11:06:46 -0800

    Add examples and load cli
    Adapt the load CLI and the example
    using pickled DF.
    
    Add JSON support

--- conftest.py
+++ conftest.py
@@ -1,13 +1,43 @@
 import os
 import pytest
+import pandas as pd
 
-from redisvl.utils.connection import get_async_redis_connection
+from redisvl.utils.connection import (
+    get_async_redis_connection,
+    get_redis_connection
+)
 
 HOST = os.environ.get("REDIS_HOST", "localhost")
 PORT = os.environ.get("REDIS_PORT", 6379)
 USER = os.environ.get("REDIS_USER", "default")
 PASS = os.environ.get("REDIS_PASSWORD", "")
 
+aredis = get_async_redis_connection(HOST, PORT, PASS)
+redis = get_redis_connection(HOST, PORT, PASS)
+print(type(redis))
+
 @pytest.fixture
-def async_redis():
-    return get_async_redis_connection(HOST, PORT, PASS)
+def async_client():
+    return aredis
+
+@pytest.fixture
+def client():
+    return redis
+
+@pytest.fixture
+def df():
+
+    data = pd.DataFrame(
+        {
+            "users": ["john", "mary", "joe"],
+            "age": [1, 2, 3],
+            "job": ["engineer", "doctor", "dentist"],
+            "credit_score": ["high", "low", "medium"],
+            "user_embedding": [
+                [0.1, 0.1, 0.5],
+                [0.1, 0.1, 0.5],
+                [0.9, 0.9, 0.1],
+            ],
+        }
+    )
+    return data
