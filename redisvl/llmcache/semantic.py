040f6002dfd910fb2fdcd975d85975a3a5361e51
Tyler Hutcherson <tyler.hutcherson@redis.com>
2023-07-31 13:55:38 -0400

    revert logic updated

--- redisvl/llmcache/semantic.py
+++ redisvl/llmcache/semantic.py
@@ -204,8 +204,9 @@ class SemanticCache(BaseLLMCache):
     def _refresh_ttl(self, key: str):
         """Refreshes the TTL for the specified key."""
         client = self._index.client
-        if client and self.ttl:
-            client.expire(key, self.ttl)
+        if client:
+            if self.ttl:
+                client.expire(key, self.ttl)
         else:
             raise RuntimeError("LLMCache is not connected to a Redis instance.")
 
