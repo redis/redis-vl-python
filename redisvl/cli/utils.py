b19adfcb1589926c70dd13f3afe2af8a810ad38e
Sam Partee <sam.partee@redis.com>
2023-08-28 22:39:48 -0700

    Add option to use url from cli

--- redisvl/cli/utils.py
+++ redisvl/cli/utils.py
@@ -1,11 +1,18 @@
 import os
 from argparse import ArgumentParser, Namespace
 
+from redisvl.cli.log import get_logger
+
+logger = get_logger("[RedisVL]")
+
 
 def create_redis_url(args: Namespace) -> str:
     env_address = os.getenv("REDIS_URL")
     if env_address:
+        logger.info(f"Using Redis address from environment variable, REDIS_URL")
         return env_address
+    elif args.url:
+        return args.url
     else:
         url = "redis://"
         if args.ssl:
@@ -24,6 +31,7 @@ def add_index_parsing_options(parser: ArgumentParser) -> ArgumentParser:
     parser.add_argument(
         "-s", "--schema", help="Path to schema file", type=str, required=False
     )
+    parser.add_argument("-u", "--url", help="Redis URL", type=str, required=False)
     parser.add_argument("--host", help="Redis host", type=str, default="localhost")
     parser.add_argument("-p", "--port", help="Redis port", type=int, default=6379)
     parser.add_argument("--user", help="Redis username", type=str, default="default")
