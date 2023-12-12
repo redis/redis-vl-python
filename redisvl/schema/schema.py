07163d840f0ef7178a11f675b3b4c99a6b3a0e52
Tyler Hutcherson <tyler.hutcherson@redis.com>
2023-12-12 11:28:32 -0500

    slight cleanup of type inferrer

--- redisvl/schema/schema.py
+++ redisvl/schema/schema.py
@@ -297,9 +297,17 @@ class IndexSchema(BaseModel):
 
 
 
-class TypeInferrer:
+import re
+from typing import Any
 
-    GEO_PATTERN = r"^\s*[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)\s*$"
+class TypeInferrer:
+    """
+    Infers the type of a field based on its value.
+    """
+
+    GEO_PATTERN = re.compile(
+        r"^\s*[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)\s*$"
+    )
 
     TYPE_METHOD_MAP = {
         "numeric": "_is_numeric",
@@ -310,14 +318,28 @@ class TypeInferrer:
 
     @classmethod
     def infer(cls, value: Any) -> str:
-        for type_name, type_check_method_name in cls.TYPE_METHOD_MAP.items():
-            type_check_method = getattr(cls, type_check_method_name)
-            if type_check_method(value):
+        """
+        Infers the field type for a given value.
+
+        Args:
+            value: The value to infer the type of.
+
+        Returns:
+            The inferred field type as a string.
+
+        Raises:
+            ValueError: If the type cannot be inferred.
+        """
+        for type_name, method_name in cls.TYPE_METHOD_MAP.items():
+            if getattr(cls, method_name)(value):
                 return type_name
         raise ValueError(f"Unable to infer type for value: {value}")
 
     @classmethod
-    def _is_numeric(cls, value) -> bool:
+    def _is_numeric(cls, value: Any) -> bool:
+        """Check if the value is numeric."""
+        if not isinstance(value, (int, float, str)):
+            return False
         try:
             float(value)
             return True
@@ -325,14 +347,16 @@ class TypeInferrer:
             return False
 
     @classmethod
-    def _is_tag(cls, value) -> bool:
+    def _is_tag(cls, value: Any) -> bool:
+        """Check if the value is a tag."""
         return isinstance(value, (list, set, tuple)) and all(isinstance(v, str) for v in value)
 
     @classmethod
-    def _is_text(cls, value) -> bool:
+    def _is_text(cls, value: Any) -> bool:
+        """Check if the value is text."""
         return isinstance(value, str)
 
     @classmethod
-    def _is_geographic(cls, value) -> bool:
-        if isinstance(value, str):
-            return bool(re.match(cls.GEO_PATTERN, value))
+    def _is_geographic(cls, value: Any) -> bool:
+        """Check if the value is a geographic coordinate."""
+        return isinstance(value, str) and cls.GEO_PATTERN.match(value) is not None
