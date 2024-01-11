8c9217dfcbaa3d73ab61d37baf677bdd48938635
Tyler Hutcherson <tyler.hutcherson@redis.com>
2024-01-11 15:25:21 -0500

    Expand query support (#95)
    This PR introduces configurable `dialect` support and a new query method
    for `pagination` / batching mechanism.

--- tests/unit/test_query_types.py
+++ tests/unit/test_query_types.py
@@ -2,7 +2,7 @@ from redis.commands.search.query import Query
 from redis.commands.search.result import Result
 
 from redisvl.index import process_results
-from redisvl.query import CountQuery, FilterQuery, VectorQuery
+from redisvl.query import CountQuery, FilterQuery, RangeQuery, VectorQuery
 from redisvl.query.filter import Tag
 
 # Sample data for testing
@@ -22,6 +22,11 @@ def test_count_query():
     assert isinstance(count_query.params, dict)
     assert count_query.params == {}
 
+    # Test set_filter functionality
+    new_filter_expression = Tag("category") == "Sportswear"
+    count_query.set_filter(new_filter_expression)
+    assert count_query.get_filter() == new_filter_expression
+
     fake_result = Result([2], "")
     assert process_results(fake_result, count_query, "json") == 2
 
@@ -30,7 +35,7 @@ def test_filter_query():
     # Create a filter expression
     filter_expression = Tag("brand") == "Nike"
     return_fields = ["brand", "price"]
-    filter_query = FilterQuery(return_fields, filter_expression, 10)
+    filter_query = FilterQuery(filter_expression, return_fields, 10)
 
     # Check properties
     assert filter_query._return_fields == return_fields
@@ -39,16 +44,74 @@ def test_filter_query():
     assert isinstance(filter_query.query, Query)
     assert isinstance(filter_query.params, dict)
     assert filter_query.params == {}
+    assert filter_query._dialect == 2
+
+    # Test set_filter functionality
+    new_filter_expression = Tag("category") == "Sportswear"
+    filter_query.set_filter(new_filter_expression)
+    assert filter_query.get_filter() == new_filter_expression
+
+    # Test set_paging functionality
+    filter_query.set_paging(5, 7)
+    assert filter_query._first == 5
+    assert filter_query._limit == 7
+    assert filter_query._num_results == 10
 
 
 def test_vector_query():
     # Create a vector query
-    vector_query = VectorQuery(sample_vector, "vector_field", ["field1", "field2"])
+    vector_query = VectorQuery(
+        sample_vector, "vector_field", ["field1", "field2"], dialect=3, num_results=10
+    )
 
     # Check properties
     assert vector_query._vector == sample_vector
     assert vector_query._field == "vector_field"
+    assert vector_query._num_results == 10
     assert "field1" in vector_query._return_fields
     assert isinstance(vector_query.query, Query)
     assert isinstance(vector_query.params, dict)
     assert vector_query.params != {}
+    assert vector_query._dialect == 3
+
+    # Test set_filter functionality
+    new_filter_expression = Tag("category") == "Sportswear"
+    vector_query.set_filter(new_filter_expression)
+    assert vector_query.get_filter() == new_filter_expression
+
+    # Test set_paging functionality
+    vector_query.set_paging(5, 7)
+    assert vector_query._first == 5
+    assert vector_query._limit == 7
+    assert vector_query._num_results == 10
+
+
+def test_range_query():
+    # Create a filter expression
+    filter_expression = Tag("brand") == "Nike"
+
+    # Create a RangeQuery instance
+    range_query = RangeQuery(
+        sample_vector, "vector_field", ["field1"], filter_expression, num_results=10
+    )
+
+    # Check properties
+    assert range_query._vector == sample_vector
+    assert range_query._field == "vector_field"
+    assert range_query._num_results == 10
+    assert range_query.distance_threshold == 0.2
+    assert "field1" in range_query._return_fields
+    assert isinstance(range_query.query, Query)
+    assert isinstance(range_query.params, dict)
+    assert range_query.params != {}
+
+    # Test set_filter functionality
+    new_filter_expression = Tag("category") == "Outdoor"
+    range_query.set_filter(new_filter_expression)
+    assert range_query.get_filter() == new_filter_expression
+
+    # Test set_paging functionality
+    range_query.set_paging(5, 7)
+    assert range_query._first == 5
+    assert range_query._limit == 7
+    assert range_query._num_results == 10
