3bed876ac6659fe7fdb06d6e3c9cfffde883da6f
Sam Partee <sam.partee@redis.com>
2024-01-18 18:34:00 -0800

    Fix jupyter dependency bug

--- redisvl/utils/utils.py
+++ redisvl/utils/utils.py
@@ -1,8 +1,6 @@
+import numpy as np
 from typing import Any, List
 
-import numpy as np
-from IPython.display import HTML, display
-from redis.commands.search.result import Result
 
 # required modules
 REDIS_REQUIRED_MODULES = [
@@ -86,46 +84,3 @@ def array_to_buffer(array: List[float], dtype: Any = np.float32) -> bytes:
 def buffer_to_array(buffer: bytes, dtype: Any = np.float32) -> List[float]:
     """Convert bytes into into a list of floats."""
     return np.frombuffer(buffer, dtype=dtype).tolist()
-
-
-def table_print(dict_list):
-    # If there's nothing in the list, there's nothing to print
-    if len(dict_list) == 0:
-        return
-
-    # Getting column names (dictionary keys) using the first dictionary
-    columns = dict_list[0].keys()
-
-    # HTML table header
-    html = "<table><tr><th>"
-    html += "</th><th>".join(columns)
-    html += "</th></tr>"
-
-    # HTML table content
-    for dictionary in dict_list:
-        html += "<tr><td>"
-        html += "</td><td>".join(str(dictionary[column]) for column in columns)
-        html += "</td></tr>"
-
-    # HTML table footer
-    html += "</table>"
-
-    # Displaying the table
-    display(HTML(html))
-
-
-def result_print(results):
-    if isinstance(results, Result):
-        # If there's nothing in the list, there's nothing to print
-        if len(results.docs) == 0:
-            return
-
-        results = [doc.__dict__ for doc in results.docs]
-
-    to_remove = ["id", "payload"]
-    for doc in results:
-        for key in to_remove:
-            if key in doc:
-                del doc[key]
-
-    table_print(results)
