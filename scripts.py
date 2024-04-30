291110ec0c7091fd08e5b7051e24c5a4b9880437
Robert Shelton <robert.b.shelton.42@gmail.com>
2024-04-30 11:09:10 -0400

    Update release.yml
    add push branch

--- scripts.py
+++ scripts.py
@@ -1,39 +1,39 @@
 import subprocess
 
 def format():
-    subprocess.run(["isort", "./redisvl", "./tests/", "--profile", "black"])
-    subprocess.run(["black", "./redisvl", "./tests/"])
+    subprocess.run(["isort", "./redisvl", "./tests/", "--profile", "black"], check=True)
+    subprocess.run(["black", "./redisvl", "./tests/"], check=True)
 
 def check_format():
-    subprocess.run(["black", "--check", "./redisvl"])
+    subprocess.run(["black", "--check", "./redisvl"], check=True)
 
 def sort_imports():
-    subprocess.run(["isort", "./redisvl", "./tests/", "--profile", "black"])
+    subprocess.run(["isort", "./redisvl", "./tests/", "--profile", "black"], check=True)
 
 def check_sort_imports():
-    subprocess.run(["isort", "./redisvl", "--check-only", "--profile", "black"])
+    subprocess.run(["isort", "./redisvl", "--check-only", "--profile", "black"], check=True)
 
 def check_lint():
-    subprocess.run(["pylint", "--rcfile=.pylintrc", "./redisvl"])
+    subprocess.run(["pylint", "--rcfile=.pylintrc", "./redisvl"], check=True)
 
 def mypy():
-    subprocess.run(["python", "-m", "mypy", "./redisvl"])
+    subprocess.run(["python", "-m", "mypy", "./redisvl"], check=True)
 
 def test():
-    subprocess.run(["python", "-m", "pytest", "--log-level=CRITICAL"])
+    subprocess.run(["python", "-m", "pytest", "--log-level=CRITICAL"], check=True)
 
 def test_verbose():
-    subprocess.run(["python", "-m", "pytest", "-vv", "-s", "--log-level=CRITICAL"])
+    subprocess.run(["python", "-m", "pytest", "-vv", "-s", "--log-level=CRITICAL"], check=True)
 
 def test_cov():
     subprocess.run(["python", "-m", "pytest", "-vv", "--cov=./redisvl", "--cov-report=xml", "--log-level=CRITICAL"], check=True)
 
 def cov():
-    subprocess.run(["coverage", "html"])
+    subprocess.run(["coverage", "html"], check=True)
     print("If data was present, coverage report is in ./htmlcov/index.html")
 
 def test_notebooks():
-    subprocess.run(["cd", "docs/", "&&", "poetry run treon", "-v"])
+    subprocess.run(["cd", "docs/", "&&", "poetry run treon", "-v"], check=True)
 
 def build_docs():
     subprocess.run("cd docs/ && make html", shell=True)
