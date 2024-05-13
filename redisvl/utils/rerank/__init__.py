d08839c2a814044dd7265d8687c2ab0c619f961c
Brian Sam-Bodden <bsb@redis.com>
2024-05-12 20:42:18 -0700

    Reranker implementation using Cross Encoders (HuggingFace / SentenceTransformers)

--- redisvl/utils/rerank/__init__.py
+++ redisvl/utils/rerank/__init__.py
@@ -1,7 +1,5 @@
 from redisvl.utils.rerank.base import BaseReranker
 from redisvl.utils.rerank.cohere import CohereReranker
+from redisvl.utils.rerank.cross_encoder import CrossEncoderReranker
 
-__all__ = [
-    "BaseReranker",
-    "CohereReranker",
-]
+__all__ = ["BaseReranker", "CohereReranker", "CrossEncoderReranker"]
--- tests/integration/test_rerankers.py
+++ tests/integration/test_rerankers.py
@@ -2,24 +2,34 @@ import os
 
 import pytest
 
-from redisvl.utils.rerank import CohereReranker
+from redisvl.utils.rerank import CohereReranker, CrossEncoderReranker
 
 
 # Fixture for the reranker instance
 @pytest.fixture
-def reranker():
+def cohereReranker():
     skip_reranker = os.getenv("SKIP_RERANKERS", "False").lower() == "true"
     if skip_reranker:
         pytest.skip("Skipping reranker instantiation...")
     return CohereReranker()
 
 
+@pytest.fixture
+def crossEncoderReranker():
+    return CrossEncoderReranker()
+
+
+@pytest.fixture
+def crossEncoderRerankerWithCustomModel():
+    return CrossEncoderReranker("cross-encoder/stsb-distilroberta-base")
+
+
 # Test for basic ranking functionality
-def test_rank_documents(reranker):
+def test_rank_documents_cohere(cohereReranker):
     docs = ["document one", "document two", "document three"]
     query = "search query"
 
-    reranked_docs, scores = reranker.rank(query, docs)
+    reranked_docs, scores = cohereReranker.rank(query, docs)
 
     assert isinstance(reranked_docs, list)
     assert len(reranked_docs) == len(docs)  # Ensure we get back as many docs as we sent
@@ -28,11 +38,11 @@ def test_rank_documents(reranker):
 
 # Test for asynchronous ranking functionality
 @pytest.mark.asyncio
-async def test_async_rank_documents(reranker):
+async def test_async_rank_documents_cohere(cohereReranker):
     docs = ["document one", "document two", "document three"]
     query = "search query"
 
-    reranked_docs, scores = await reranker.arank(query, docs)
+    reranked_docs, scores = await cohereReranker.arank(query, docs)
 
     assert isinstance(reranked_docs, list)
     assert len(reranked_docs) == len(docs)  # Ensure we get back as many docs as we sent
@@ -40,17 +50,47 @@ async def test_async_rank_documents(reranker):
 
 
 # Test handling of bad input
-def test_bad_input(reranker):
+def test_bad_input_cohere(cohereReranker):
     with pytest.raises(Exception):
-        reranker.rank("", [])  # Empty query or documents
+        cohereReranker.rank("", [])  # Empty query or documents
 
     with pytest.raises(Exception):
-        reranker.rank(123, ["valid document"])  # Invalid type for query
+        cohereReranker.rank(123, ["valid document"])  # Invalid type for query
 
     with pytest.raises(Exception):
-        reranker.rank("valid query", "not a list")  # Invalid type for documents
+        cohereReranker.rank("valid query", "not a list")  # Invalid type for documents
 
     with pytest.raises(Exception):
-        reranker.rank(
+        cohereReranker.rank(
             "valid query", [{"field": "valid document"}], rank_by=["invalid_field"]
         )  # Invalid rank_by field
+
+
+def test_rank_documents_cross_encoder(crossEncoderReranker):
+    query = "I love you"
+    texts = ["I love you", "I like you", "I don't like you", "I hate you"]
+    reranked_docs, scores = crossEncoderReranker.rank(query, texts)
+
+    for i in range(min(len(texts), crossEncoderReranker.limit) - 1):
+        assert scores[i] > scores[i + 1]
+
+
+def test_rank_documents_cross_encoder_custom_model(crossEncoderRerankerWithCustomModel):
+    query = "I love you"
+    texts = ["I love you", "I like you", "I don't like you", "I hate you"]
+    reranked_docs, scores = crossEncoderRerankerWithCustomModel.rank(query, texts)
+
+    for i in range(min(len(texts), crossEncoderRerankerWithCustomModel.limit) - 1):
+        assert scores[i] > scores[i + 1]
+
+
+@pytest.mark.asyncio
+async def test_async_rank_cross_encoder(crossEncoderReranker):
+    docs = ["document one", "document two", "document three"]
+    query = "search query"
+
+    reranked_docs, scores = await crossEncoderReranker.arank(query, docs)
+
+    assert isinstance(reranked_docs, list)
+    assert len(reranked_docs) == len(docs)  # Ensure we get back as many docs as we sent
+    assert all(isinstance(score, float) for score in scores)  # Scores should be floats
