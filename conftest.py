1784e5a4ccff9a13b5ae2bd5e9b15cec139e712d
Tyler Hutcherson <tyler.hutcherson@redis.com>
2024-06-04 09:57:45 -0400

    cleanup

--- conftest.py
+++ conftest.py
@@ -6,12 +6,6 @@ from redisvl.redis.connection import RedisConnectionFactory
 from testcontainers.compose import DockerCompose
 
 
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
-
 
 @pytest.fixture(scope="session", autouse=True)
 def redis_container():
