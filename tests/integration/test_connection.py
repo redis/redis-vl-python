934d2691ed874bb88129a3885e92ca0c0d61e2a9
Tyler Hutcherson <tyler.hutcherson@redis.com>
2024-06-06 20:32:34 -0400

    Add client info reporting (#155)
    We need to be able to track the usage of clients for measuring adoption
    and impact. The `CLIENT SETINFO` command can be used for this purpose.
    The required string format is documented
    [here](https://redis.io/docs/latest/commands/client-setinfo/#:~:text=The%20CLIENT%20SETINFO%20command%20assigns,CLIENT%20LIST%20and%20CLIENT%20INFO%20).
    
    ### Variants
    
    - Standalone RedisVL:
    ```
    CLIENT SETINFO LIB-NAME redis-py(redisvl_v0.2.0)
    CLIENT SETINFO LIB-VER 5.0.4
    ```
    
    - Abstraction layers (like LlamaIndex or LangChain):
    ```
    CLIENT SETINFO LIB-NAME redis-py(redisvl_v0.2.0;llama-index-vector-stores-redis_v0.1.0)
    CLIENT SETINFO LIB-VER 5.0.4
    ```
    
    ### Constraints
    - RedisVL uses both Async connection and standard connection instances
    from Redis Py
    	- So the technique to run this command needs to properly handle this...
    - Other wrappers around RedisVL like LangChain and LlamaIndex will need
    to pass through their lib_name
    - These libraries generally support the notion of providing your own
    client instance OR providing the connection string and performing the
    connection on your behalf -- which also adds some difficulty.
    
    
    ### Planned Route
    
    In order to build the proper name string, all clients that wrap redis-py
    will need to use the following format:
    ```
    {package-name}_v{version}
    ```
    
    RedisVL will implement the default which is `redisvl_v0.x.x`, but outer
    wrappers of RedisVL can implement their own by using a lib_name `kwarg`
    on the index class like
    
    ```
    SearchIndex(lib_name="langchain_v0.2.1")
    ```

--- tests/integration/test_connection.py
+++ tests/integration/test_connection.py
@@ -6,13 +6,45 @@ from redis.asyncio import Redis as AsyncRedis
 from redis.exceptions import ConnectionError
 
 from redisvl.redis.connection import RedisConnectionFactory, get_address_from_env
+from redisvl.version import __version__
+
+EXPECTED_LIB_NAME = f"redis-py(redisvl_v{__version__})"
+
+
+def compare_versions(version1, version2):
+    """
+    Compare two Redis version strings numerically.
+
+    Parameters:
+    version1 (str): The first version string (e.g., "7.2.4").
+    version2 (str): The second version string (e.g., "6.2.1").
+
+    Returns:
+    int: -1 if version1 < version2, 0 if version1 == version2, 1 if version1 > version2.
+    """
+    v1_parts = list(map(int, version1.split(".")))
+    v2_parts = list(map(int, version2.split(".")))
+
+    for v1, v2 in zip(v1_parts, v2_parts):
+        if v1 < v2:
+            return False
+        elif v1 > v2:
+            return True
+
+    # If the versions are equal so far, compare the lengths of the version parts
+    if len(v1_parts) < len(v2_parts):
+        return False
+    elif len(v1_parts) > len(v2_parts):
+        return True
+
+    return True
 
 
 def test_get_address_from_env(redis_url):
     assert get_address_from_env() == redis_url
 
 
-def test_sync_redis_connection(redis_url):
+def test_sync_redis_connect(redis_url):
     client = RedisConnectionFactory.connect(redis_url)
     assert client is not None
     assert isinstance(client, Redis)
@@ -21,7 +53,7 @@ def test_sync_redis_connection(redis_url):
 
 
 @pytest.mark.asyncio
-async def test_async_redis_connection(redis_url):
+async def test_async_redis_connect(redis_url):
     client = RedisConnectionFactory.connect(redis_url, use_async=True)
     assert client is not None
     assert isinstance(client, AsyncRedis)
@@ -49,11 +81,19 @@ def test_unknown_redis():
         bad_client.ping()
 
 
-def test_required_modules(client):
-    RedisConnectionFactory.validate_redis_modules(client)
+def test_validate_redis(client):
+    redis_version = client.info()["redis_version"]
+    if not compare_versions(redis_version, "7.2.0"):
+        pytest.skip("Not using a late enough version of Redis")
+    RedisConnectionFactory.validate_redis(client)
+    lib_name = client.client_info()
+    assert lib_name["lib-name"] == EXPECTED_LIB_NAME
 
 
-@pytest.mark.asyncio
-async def test_async_required_modules(async_client):
-    client = await async_client
-    RedisConnectionFactory.validate_async_redis_modules(client)
+def test_validate_redis_custom_lib_name(client):
+    redis_version = client.info()["redis_version"]
+    if not compare_versions(redis_version, "7.2.0"):
+        pytest.skip("Not using a late enough version of Redis")
+    RedisConnectionFactory.validate_redis(client, "langchain_v0.1.0")
+    lib_name = client.client_info()
+    assert lib_name["lib-name"] == f"redis-py(redisvl_v{__version__};langchain_v0.1.0)"
