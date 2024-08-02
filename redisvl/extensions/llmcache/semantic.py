38bb5df78b6503c5e3a01f3dcf561ee62e57dbbe
Tyler Hutcherson <tyler.hutcherson@redis.com>
2024-08-01 22:44:54 -0400

    debug

--- redisvl/extensions/llmcache/semantic.py
+++ redisvl/extensions/llmcache/semantic.py
@@ -300,6 +300,8 @@ class SemanticCache(BaseLLMCache):
             key = cache_search_result["id"]
             self._refresh_ttl(key)
 
+            print(cache_search_result, flush=True)
+
             # Create cache hit
             cache_hit = CacheHit(**cache_search_result)
             cache_hit_dict = {
