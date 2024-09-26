59b79669e7b4692d41bbd46a4b1cf800ce0347da
fzowl <zoltan@voyageai.com>
2024-09-26 19:59:12 +0200

    Corrections

--- redisvl/utils/rerank/voyageai.py
+++ redisvl/utils/rerank/voyageai.py
@@ -120,7 +120,10 @@ class VoyageAIReranker(BaseReranker):
                 if isinstance(doc, dict) and "content" in doc
             ]
         else:
-            texts = [str(doc) for doc in docs]
+            raise ValueError(
+                "The docs should either be List[str] or List[Dict[str, Any]] - "
+                "this case, the documents to rerank has to be in the 'content'"
+            )
 
         reranker_kwargs = {
             "query": query,
