97b9b6811dbf8f7ebadc6eacd2eb44c3510a7ee5
Andrew Brookins <a.m.brookins@gmail.com>
2025-01-27 11:53:33 -0800

    Add a default for the `dtype` field of `BaseVectorizer` (#261)
    In 0.3.8, we introduced a new field to `BaseVectorizer`, `dtype`, but
    did not give it a default value. This broke downstream software written
    against previous versions of redisvl that lacked this field.
    
    Fixes
    [langchain/langchain-redis#48](https://github.com/langchain-ai/langchain-redis/issues/48).
    
    ---------
    
    Co-authored-by: Tyler Hutcherson <tyler.hutcherson@redis.com>

---
+++ tests/unit/test_base_vectorizer.py
@@ -0,0 +1,32 @@
+from typing import List
+from redisvl.utils.vectorize.base import BaseVectorizer
+
+
+
+def test_base_vectorizer_defaults():
+    """
+    Test that the base vectorizer defaults are set correctly, with 
+    a default for dtype. Versions before 0.3.8 did not have this field.
+
+    A regression test for langchain-redis/#48
+    """
+    class SimpleVectorizer(BaseVectorizer):
+        model: str = "simple"
+        dims: int = 10
+        
+        def embed(self, text: str, **kwargs) -> List[float]:
+            return [0.0] * self.dims
+        
+        async def aembed(self, text: str, **kwargs) -> List[float]:
+            return [0.0] * self.dims
+        
+        async def aembed_many(self, texts: List[str], **kwargs) -> List[List[float]]:
+            return [[0.0] * self.dims] * len(texts)
+        
+        def embed_many(self, texts: List[str], **kwargs) -> List[List[float]]:
+            return [[0.0] * self.dims] * len(texts)
+
+    vectorizer = SimpleVectorizer()
+    assert vectorizer.model == "simple"
+    assert vectorizer.dims == 10
+    assert vectorizer.dtype == "float32"
