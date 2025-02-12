44f4ba88fc01015ff6e416db979d94e1e69bba82
Tyler Hutcherson <tyler.hutcherson@redis.com>
2025-02-12 09:57:14 -0500

    tweak pytest config

--- scripts.py
+++ scripts.py
@@ -30,22 +30,20 @@ def check_mypy():
 
 
 def test():
+    test_cmd = ["python", "-m", "pytest", "-n", "auto", "--log-level=CRITICAL"]
     # Get any extra arguments passed to the script
     extra_args = sys.argv[1:]
-    if not extra_args:
-        test_cmd = ["python", "-m", "pytest", "-n", "auto", "--log-level=CRITICAL"]
-    else:
-        test_cmd = ["python", "-m", "pytest", "-n", "auto", "--log-level=CRITICAL"] + extra_args
+    if extra_args:
+        test_cmd.extend(extra_args)
     subprocess.run(test_cmd, check=True)
 
 
 def test_verbose():
+    test_cmd = ["python", "-m", "pytest", "-n", "auto", "-vv", "-s", "--log-level=CRITICAL"]
     # Get any extra arguments passed to the script
     extra_args = sys.argv[1:]
-    if not extra_args:
-        test_cmd = ["python", "-m", "pytest", "-n", "auto", "-vv", "-s", "--log-level=CRITICAL"]
-    else:
-        test_cmd = ["python", "-m", "pytest", "-n", "auto", "-vv", "-s", "--log-level=CRITICAL"] + extra_args
+    if extra_args:
+        test_cmd.extend(extra_args)
     subprocess.run(test_cmd, check=True)
 
 
