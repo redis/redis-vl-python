dc657d2ee1fd424a53b5a899d00db9f9809b0e94
Justin Cechmanek <165097110+justin-cechmanek@users.noreply.github.com>
2025-04-22 10:10:02 -0700

    Rename session manager classes to message history (#323)
    This PR renames `StandardSessionManager` and `SemanticSessionManager` to
    `MessageHistory` and `SemanticMessageHistory`, respectively.
    
    This is done to improve clarity on what these classes are for.
    
    Importing `StandardSessionManager` and `SemanticSessionManager` are now
    deprecated.

--- redisvl/extensions/session_manager/__init__.py
+++ redisvl/extensions/session_manager/__init__.py
@@ -1,5 +1,23 @@
+"""
+RedisVL Session Manager Extensions (Deprecated Path)
+
+This module is kept for backward compatibility. Please use `redisvl.extensions.message_history` instead.
+"""
+
+import warnings
+
 from redisvl.extensions.session_manager.base_session import BaseSessionManager
 from redisvl.extensions.session_manager.semantic_session import SemanticSessionManager
 from redisvl.extensions.session_manager.standard_session import StandardSessionManager
+
+warnings.warn(
+    "Importing from redisvl.extensions.session_manager is deprecated. "
+    "StandardSessionManager has been renamed to MessageHistory. "
+    "SemanticSessionManager has been renamed to SemanticMessageHistory. "
+    "Please import from redisvl.extensions.message_history instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 
 __all__ = ["BaseSessionManager", "StandardSessionManager", "SemanticSessionManager"]
