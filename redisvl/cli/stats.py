8435e05accae5919c9a20628ff46e3af467a1c10
Justin Cechmanek <165097110+justin-cechmanek@users.noreply.github.com>
2025-04-30 12:13:08 -0700

    Remove non-essential tabulate and coloredlogs dependencies (#329)

--- redisvl/cli/stats.py
+++ redisvl/cli/stats.py
@@ -2,8 +2,6 @@ import argparse
 import sys
 from argparse import Namespace
 
-from tabulate import tabulate
-
 from redisvl.cli.utils import add_index_parsing_options, create_redis_url
 from redisvl.index import SearchIndex
 from redisvl.schema.schema import IndexSchema
@@ -92,11 +90,11 @@ def _display_stats(index_info, output_format="rounded_outline"):
 
     # Display the statistics in tabular format
     print("\nStatistics:")
-    print(
-        tabulate(
-            stats_data,
-            headers=["Stat Key", "Value"],
-            tablefmt=output_format,
-            colalign=("left", "left"),
-        )
-    )
+    max_key_length = max(len(key) for key, _ in stats_data)
+    horizontal_line = "─" * (max_key_length + 2)
+    print(f"╭{horizontal_line}┬────────────╮")  # top row
+    print("│ Stat Key                    │ Value      │")  # header row
+    print(f"├{horizontal_line}┼────────────┤")  # separator row
+    for key, value in stats_data:
+        print(f"│ {key:<27} │ {value[0:10]:<10} │")  # data rows
+    print(f"╰{horizontal_line}┴────────────╯")  # bottom row
