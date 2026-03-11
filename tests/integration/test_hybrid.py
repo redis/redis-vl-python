9e212b657cdb53d06dfb018d4dcacc93fdbb9bf2
Tyler Hutcherson <tyler.hutcherson@redis.com>
2026-03-11 19:54:03 -0400

    Revert test change on hybrid query

--- tests/integration/test_hybrid.py
+++ tests/integration/test_hybrid.py
@@ -136,7 +136,7 @@ def test_hybrid_query(index):
 
     results = index.query(hybrid_query)
     assert isinstance(results, list)
-    assert len(results) == index.info()["num_docs"]
+    assert len(results) == 10
     for doc in results:
         assert doc["user"] in [
             "john",
